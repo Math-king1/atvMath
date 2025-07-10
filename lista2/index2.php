a25e3021029df68846614b2eacde06aab476432d
MathKing1 <matheus_n_cardoso@estudante.sesisenai.org.br>
2025-07-10 09:31:58 -0300

    add

---
+++ lista2/index2.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular a tabuada de um número informado.</title>
+</head>
+<body>
+    
+</body>
+</html>
